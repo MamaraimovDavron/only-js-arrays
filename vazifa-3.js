eff877e494bd53387d27b178dbb5afbac066cec2
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-05-23 11:52:47 +0500

    vazifa-3 done

---
+++ vazifa-3.js
@@ -0,0 +1,8 @@
+let arr1 = [1,3,4,5,6];
+let arr2 = [2,3,45,5,7,6];
+
+if(arr1[0] === arr2[0] || arr1[arr1.length - 1] === arr2[arr2.length - 1]){
+    console.log(true);
+}
+
+else console.log(false);
